3abf331149eac03fb6783af51387c49709b15629
ParkGeunYeong <201645012@itc.ac.kr>
2019-04-07 20:56:49 +0900

    aa

---
+++ 1339.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <map>
+using namespace std;
+
+int main() {
+	int n;
+	string input;
+	cin >> n;
+	vector<pair<string,int>> b(n);
+	map<char, char> match;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> b[i].first;
+		b[i].second = size(b[i].first);
+		for (char j : b[i].first) {
+			if (match.find(j) == match.end())
+				match[j] = 0;-
+		}
+	}
+
+
+	return 0;
+}
